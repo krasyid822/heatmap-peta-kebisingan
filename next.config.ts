684cba59b45a1d9788ec662b8f5c2391bd1882e6
krasyid822 <krasyid822@gmail.com>
2026-05-27 06:42:50 +0700

    add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.1.7', '192.168.1.7:3000'],
 };
 
 export default nextConfig;
